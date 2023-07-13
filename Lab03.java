e566832735efa39457ffc474fed7fc940ad08cc2
thisassumanasekara <130208485+thisassumanasekara@users.noreply.github.com>
2023-07-13 09:39:52 +0530

    Add files via upload

---
+++ Lab03.java
@@ -0,0 +1,29 @@
+package com.mycompany.lab03;
+
+public class Lab03 {
+
+    private int location;
+    private String description;
+    
+    public void setValues (int l, String d)
+    {
+        this.location=location;
+        this.description=description;
+    }
+    public void setLocation(int il)
+    {
+        location=il;
+    }
+    public int getLocation(int il)
+    {
+        return il;
+    }
+    public void setDescription(String ds)
+    {
+        description=ds;
+    }
+    public String getDescription(String ds)
+    {
+        return ds;
+    }
+}
